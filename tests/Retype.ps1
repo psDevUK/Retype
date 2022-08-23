61c9b8422e11d454d10842be2754cde3a20ff4ea
Adam <adambacon1@hotmail.com>
2022-08-24 00:42:42 +0100

    Update Retype.ps1

--- tests/Retype.ps1
+++ tests/Retype.ps1
@@ -1,12 +1,10 @@
 BeforeAll {
-      Import-Module ./YoutubeDownloader/YoutubeDownloader.psd1
+      Import-Module ./Retype/Retype/Retype.psd1
 }
 
-Describe 'Save-YoutubeVideo' {
-      It 'Returns all expected properties' {
-            $null = Save-YoutubeVideo -URL "https://www.youtube.com/watch?v=0EqSXDwTq6U" -Path /tmp/
-            $video = Get-ChildItem -Path /tmp/ -Filter *.mp4 | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-            $video.FullName | Should -Be "/tmp/Charlie bit my finger! ORIGINAL.mp4"
-            $video.Length | Should -BeGreaterThan 0
+Describe 'Install-Retype' {
+      It 'Makes sure Retype has installed' {
+            Install-Retype
+            $env:Path -match 'Retype' | Should -Be $true
       }
 }
