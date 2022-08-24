071bb14a45957d98309aa6d579e2682c22b52554
Adam <adambacon1@hotmail.com>
2022-08-24 01:51:37 +0100

    Update Retype.ps1

--- tests/Retype.ps1
+++ tests/Retype.ps1
@@ -1,10 +1,16 @@
 BeforeAll {
       Import-Module .\Retype\Retype.psd1
+      $AddRetype = ';.\Retype'
+      $Environment = $Environment.Insert($Environment.Length, $AddRetype)
+      [System.Environment]::SetEnvironmentVariable('Path', $Environment, 'Machine')
 }
 
 Describe 'Install-Retype' {
       It 'Makes sure Retype has installed' {
+            
             Install-Retype
+            $Environment = [System.Environment]::GetEnvironmentVariable('Path', 'Machine')
+            $Environment.length | Should -BeGreaterThan 10
             $env:Path -match 'Retype' | Should -Be $true
       }
 }
