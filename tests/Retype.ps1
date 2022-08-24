e12e450e1b4d0e1de6c0a1fdb63983ce044aedba
Adam <adambacon1@hotmail.com>
2022-08-24 02:06:54 +0100

    Update Retype.ps1

--- tests/Retype.ps1
+++ tests/Retype.ps1
@@ -5,6 +5,7 @@ BeforeAll {
 Describe 'Install-Retype' {
       It 'Makes sure Retype has installed' {
             $AddRetype = ';D:\a\Retype\Retype\Retype'
+            $Environment = [System.Environment]::GetEnvironmentVariable('Path', 'Machine')
             $Environment = $Environment.Insert($Environment.Length, $AddRetype)
             [System.Environment]::SetEnvironmentVariable('Path', $Environment, 'Machine')
             Install-Retype
